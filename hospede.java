35375e013d0ffaa7b304e74d93e7db0a33e48e2d
Maria-coder241 <mariaeduardaaraujovaurek04@gmail.com>
2025-11-08 22:49:41 -0300

    Create hospede.java

---
+++ hospede.java
@@ -0,0 +1,14 @@
+package hospede.java;
+
+public class hospede {
+    private String nome;
+    private String cpf;
+
+    public hospede(){
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public String getNome(){ return this.nome; }
+    public String getCpf(){ return this.cpf; }
+}
